203442dff0ca0733d21785e628ee00a85543255c
RubenDelgadoPareja <ruben051199@gmail.com>
2024-02-04 17:55:21 +0100

    ISSUE-72: Eliminamos los getters que no hacián falta. Dejamos la clase DDD, ya que es una práctica común generar un Identificador Unico propio por cada clase, dandole la caracteristica de la identidad. closes #72

--- src/modules/appointment/domain/appointment.ts
+++ src/modules/appointment/domain/appointment.ts
@@ -1,15 +1,9 @@
 import { Entity } from '../../shared/domain/entity';
 import { UniqueEntityID } from '../../shared/domain/unique-entity-id';
-import { AppointmentDate } from './appointment-date';
-import { AppointmentDescription } from './appointment-description';
 import { AppointmentId } from './appointment-id';
-import { AppointmentPriority } from './appointment-priority';
 
 export interface AppointmentProps {
   id: AppointmentId;
-  description: AppointmentDescription;
-  date: AppointmentDate;
-  priority: AppointmentPriority;
 }
 
 export class Appointment extends Entity<AppointmentProps> {
@@ -22,21 +16,5 @@ export class Appointment extends Entity<AppointmentProps> {
     id?: UniqueEntityID,
   ): Appointment {
     return new Appointment(props, id);
-  }
-
-  get AppointmentId(): AppointmentId {
-    return AppointmentId.create(this._id);
-  }
-
-  get AppointmentDescription(): AppointmentDescription {
-    return this.props.description;
-  }
-
-  get AppointmentDate(): AppointmentDate {
-    return this.props.date;
-  }
-
-  get AppointmentPriority(): AppointmentPriority {
-    return this.props.priority;
   }
 }
