0d9688f0564a0f6a0fb06dd2c83516b936e3f61b
RubenDelgadoPareja <ruben051199@gmail.com>
2024-03-10 16:33:40 +0100

    ISSUE #81: Comentar en el test de paciente ,que es un test de prueba para probar la configuración de la herramienta. closes #81

--- src/tests/patient/patient.test.ts
+++ src/tests/patient/patient.test.ts
@@ -1,6 +1,7 @@
 import { Patient } from '../../modules/patient/domain/patient';
 import { UniqueEntityID } from '../../modules/shared/domain/unique-entity-id';
 
+// Test de prueba para la configuración de la herramienta
 describe('Patiente', () => {
   test('Create patient', () => {
     const id = new UniqueEntityID();
