4a7fa8b5280f62381144b930cf42ed2a65efcbbd
RubenDelgadoPareja <ruben051199@gmail.com>
2024-06-13 20:44:32 +0200

    issue #97: Replantear el retardo estimado del usuario y hacerlo un Enum

--- src/modules/patient/domain/patient.ts
+++ src/modules/patient/domain/patient.ts
@@ -4,15 +4,24 @@ export enum PatientRisk {
   MEDIUM = 'medium',
   HIGH = 'high',
 }
+
+export enum Delay {
+  IMMEDIATELY = 'immediately',
+  FIFTEEN_MINUTES = '15 minutes',
+  SIXTY_MINUTES = '60 minutes',
+  ONE_HUNDRED_TWENTY_MINUTES = '120 minutes',
+  TWO_HUNDERED_FOURTY_MINUTES = '240 minutes',
+}
+
 export class Patient {
   constructor(
     public readonly name: string,
     public readonly dni: string,
     public readonly ssc: string,
     public readonly risk: PatientRisk,
-    public readonly delay: number,
+    public readonly delay: Delay,
+    // TODO: Create issue and implement the triage form model
     public readonly triageForm: unknown,
-    public readonly queuePosition: number,
   ) {}
 
   getName(): string {
@@ -31,15 +40,11 @@ export class Patient {
     return this.risk;
   }
 
-  getDelay(): number {
+  getDelay(): Delay {
     return this.delay;
   }
 
   getTriageForm(): unknown {
     return this.triageForm;
-  }
-
-  getQueuePosition(): number {
-    return this.queuePosition;
   }
 }
