a162d123ac24911c8e13aa953054de70460e9849
RubenDelgadoPareja <ruben051199@gmail.com>
2024-07-08 20:39:34 +0200

    ✨ Eliminar cosas innecesarios. Dejamos esta tarea bloqueada. issue Modelado del cuestionario de síntomas #110

---
+++ src/modules/medicalForm/domain/medicalForm.ts
@@ -0,0 +1,13 @@
+
+export class SymptomsForm {
+    private constructor(
+        private readonly id: number,
+        private readonly patientId: number,
+        private readonly reason: string,
+        private readonly discriminatory: string,
+        private readonly symptoms: string,
+        // private readonly vitalSigns: unknown,
+        private readonly diagnosis: string
+    ) {}
+
+}
