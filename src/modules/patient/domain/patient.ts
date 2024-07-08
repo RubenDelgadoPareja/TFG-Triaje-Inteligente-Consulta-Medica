654cafc7b65bc06b60b627e242395133dbde04a1
RubenDelgadoPareja <ruben051199@gmail.com>
2024-07-08 20:29:02 +0200

    ✨Corrección de comentarios. issue Análisis del modelo de Paciente #97

--- src/modules/patient/domain/patient.ts
+++ src/modules/patient/domain/patient.ts
@@ -1,17 +1,7 @@
-export interface PatientInputProps{
-  name: string;
-  dni: string;
-  turn: unknown;
-  symptomForm: unknown;
-}
 
 export class Patient {
-  readonly __brand = "Patient" as const;
-
-  private constructor(private readonly props: PatientInputProps) {}
-
-  static create(props: PatientInputProps): Patient {
-    // TODO: Create custom error message for invalid patient creation
-    return new Patient(props);
-  }
+  private constructor(
+    private readonly name: string,
+    private readonly dni: string,
+  ){}
 }
