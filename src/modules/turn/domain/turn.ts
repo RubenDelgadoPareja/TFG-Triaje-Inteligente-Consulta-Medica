c82ba1a41c1d6e4d6e7e085ccfbfe2b01c6ba0c8
RubenDelgadoPareja <ruben051199@gmail.com>
2024-07-08 20:24:32 +0200

    ✨ Corrección de cometarios. issue Modelado del turno del paciente #113

--- src/modules/turn/domain/turn.ts
+++ src/modules/turn/domain/turn.ts
@@ -1,13 +1,5 @@
-
+import { Patient } from './../../patient/domain/patient';
 import { DateTime } from 'luxon';
-
-export interface TurnInputProps {
-  id: number;
-  patientId: number,
-  position: number,
-  startedAt: DateTime,
-  risk: RiskEnum,
-}
 
 export enum RiskEnum {
   LOW = 'LOW',
@@ -15,37 +7,11 @@ export enum RiskEnum {
   HIGH = 'HIGH',
   CRITICAL = 'CRITICAL',
 }
-
 export class Turn {
-  readonly __brand = "Turn" as const;
+  private constructor(
+    private readonly patient: Patient,
+    private readonly startedAt: DateTime,
+    private readonly risk: RiskEnum,
+  ) {}
 
-  private constructor(private readonly props: TurnInputProps) {}
-
-  static create(props: TurnInputProps): Turn {
-    return new Turn(props);
-  }
-
-  get id(): number {
-    return this.props.id;
-  }
-
-  get patientId(): number {
-    return this.props.patientId;
-  }
-
-  get position(): number {
-    return this.position;
-  }
-
-  get startedAt(): DateTime {
-    return this.startedAt;
-  }
-
-  get waitingTime(): number {
-    return DateTime.now().diff(this.startedAt, 'minutes').minutes;
-  }
-
-  get risk(): RiskEnum {
-    return this.risk;
-  }
 }
