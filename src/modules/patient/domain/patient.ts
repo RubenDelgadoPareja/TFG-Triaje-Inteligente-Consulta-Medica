4d8d1f58bbaad02065cba2e477f4178086314c27
RubenDelgadoPareja <ruben051199@gmail.com>
2024-07-08 19:12:19 +0200

    ♻️ Refactor modelo de Paciente, hemos eliminado atributos inecesarios que no aportan valor actualmetne como id o ssn , ya tenemos el dni. Eliminamos los getters porque no se usan. issue #97

--- src/modules/patient/domain/patient.ts
+++ src/modules/patient/domain/patient.ts
@@ -1,8 +1,6 @@
 export interface PatientInputProps{
-  id: number;
   name: string;
   dni: string;
-  ssc: string;
   turn: unknown;
   symptomForm: unknown;
 }
@@ -15,29 +13,5 @@ export class Patient {
   static create(props: PatientInputProps): Patient {
     // TODO: Create custom error message for invalid patient creation
     return new Patient(props);
-  }
-
-  get id(): number {
-    return this.props.id;
-  }
-
-  get name(): string {
-    return this.props.name;
-  }
-
-  get dni(): string {
-    return this.props.dni;
-  }
-
-  get ssc(): string {
-    return this.props.ssc;
-  }
-
-  get turn(): unknown {
-    return this.props.turn;
-  }
-
-  get symptomForm(): unknown {
-    return this.props.symptomForm;
   }
 }
